b83c66abc089e9d0d76ec3211928190794c8cf86
celsorodrigo <celsorodrigoma@gmail.com>
2022-07-18 15:27:52 -0300

    Concluído 1º requisito.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+// import React from 'react';
+// import { screen, render } from '@testing-library/react';
+
+// describe('Tests of component APP (Requisito 1)', () => {
+//   it('', () => {
+
+//   });
+// });
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,23 @@
-test('', () => {});
-// Iniciado projeto.
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+describe('Tests of component APP (Requisito 1)', () => {
+  renderWithRouter(<App />);
+  const homeLink = screen.getByRole('link', { name: /home/i });
+  const aboutLink = screen.getByRole('link', { name: /about/i });
+  const favPokeLink = screen.getByRole('link', { name: /favorite pokémons/i });
+
+  it('Should exist 3 nav links on the header: Home, About and Favorite Pokémons.', () => {
+    expect(homeLink).toBeDefined();
+    expect(aboutLink).toBeDefined();
+    expect(favPokeLink).toBeDefined();
+  });
+
+  it('Should have corresponding text in each link.', () => {
+    expect(homeLink).toHaveTextContent(/home/i);
+    expect(aboutLink).toHaveTextContent(/about/i);
+    expect(favPokeLink).toHaveTextContent(/favorite pokémons/i);
+  });
+});
