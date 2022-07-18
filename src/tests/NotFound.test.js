e9a430a7d1a1a2baaee110ec08c30723e5cf43dc
celsorodrigo <celsorodrigoma@gmail.com>
2022-07-18 18:48:48 -0300

    Corrigido erro de lint.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getByTitle, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 
